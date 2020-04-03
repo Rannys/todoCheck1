0c46d66be68aa877e5ff1be41c8be43530738200
Ben Ali Slim <60698871+Rannys@users.noreply.github.com>
2020-04-03 10:04:38 +0100

    Update make-it-modular.js

--- make-it-modular.js
+++ make-it-modular.js
@@ -1,9 +1,9 @@
 var dir = process.argv[2];
-var filter = process.argv[3];
+var fil = process.argv[3];
 var mymodule = require('./mymodule.js')
 
 
-mymodule (dir,filter, function (err, data) {
+mymodule (dir,fil, function (err, data) {
     if (err) {
         console.log("There was an error")
     }
